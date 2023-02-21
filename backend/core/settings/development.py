5683d6b4c70dfad43c5a17921c5d347d8d672915
batyrnurhan <117359037+batyrnurkhan@users.noreply.github.com>
2023-02-21 21:25:22 +0600

    user-auth register

--- backend/core/settings/development.py
+++ backend/core/settings/development.py
@@ -1,5 +1,15 @@
 from .base import *
 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = venv("EMAIL_HOST")
+EMAIL_USE_TLS = True
+EMAIL_PORT = venv("EMAIL_PORT")
+EMAIL_HOST_USER = venv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = venv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = "batyrnurhan@gmail.com"
+DOMAIN = venv("DOMAIN")
+SITE_NAME = "HEES"
+
 DATABASES = {
     'default': {
         'ENGINE': venv('POSTGRES_ENGINE'),
