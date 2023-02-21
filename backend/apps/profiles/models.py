50040294ba14b13f06238dab870e6cbf9e44562b
batyrnurhan <117359037+batyrnurkhan@users.noreply.github.com>
2023-02-21 20:34:15 +0600

    add profile model and signals

--- backend/apps/profiles/models.py
+++ backend/apps/profiles/models.py
@@ -1,3 +1,68 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django_countries.fields import CountryField
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+from apps.common.models import TimeStampedUUIDModel
+
+User = get_user_model()
+
+
+class Gender(models.TextChoices):
+    MALE = "Male", _("Male")
+    FEMALE = "Female", _("Female")
+    OTHER = "Other", _("Other")
+
+
+class Profile(TimeStampedUUIDModel):
+    user = models.OneToOneField(User, related_name="profile", on_delete=models.CASCADE)
+    phone_number = PhoneNumberField(
+        verbose_name=_("Phone Number"), max_length=30, default="+7 777 777 77 77"
+    )
+    about_me = models.TextField(
+        verbose_name=_("About me"), default="say something about yourself"
+    )
+    license = models.CharField(
+        verbose_name=_("Teaching license"), max_length=20, blank=True, null=True
+    )
+    profile_photo = models.ImageField(
+        verbose_name=_("Profile Photo"), default="/profile_default.png"
+    )
+    gender = models.CharField(
+        verbose_name=_("Gender"),
+        choices=Gender.choices,
+        default=Gender.OTHER,
+        max_length=20,
+    )
+    country = CountryField(
+        verbose_name=_("Country"), default="KZ", blank=False, null=False
+    )
+    city = models.CharField(
+        verbose_name=_("City"),
+        max_length=180,
+        default="Almaty",
+        blank=False,
+        null=False,
+    )
+    is_teacher = models.BooleanField(
+        verbose_name=_("Teacher"),
+        default=False,
+        help_text=_("Are you wanna teach?"),
+    )
+    is_university = models.BooleanField(
+        verbose_name=_("University"),
+        default=False,
+        help_text=_("Are you wanna seek teachers?"),
+    )
+    is_agent = models.BooleanField(
+        verbose_name=_("Agent"), default=False, help_text=_("Are you an agent?")
+    )
+    top_agent = models.BooleanField(verbose_name=_("Top Agent"), default=False)
+    rating = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
+    num_reviews = models.IntegerField(
+        verbose_name=_("Number of Reviews"), default=0, null=True, blank=True
+    )
+
+    def __str__(self):
+        return f"{self.user.username}'s profile"
