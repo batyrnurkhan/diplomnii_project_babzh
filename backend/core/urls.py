cbb9b41bdfa2bc530bbea449349fb146a84cbe20
batyrnurhan <117359037+batyrnurkhan@users.noreply.github.com>
2023-02-21 20:24:38 +0600

    add custom user model

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -6,3 +6,7 @@ from django.urls import path,include
 urlpatterns = [
     path('supersecret/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+admin.site.site_header= 'HEES Admin'
+admin.site.site_title= 'HEES Admin Portal'
+admin.site.index_title= 'Welcome to the HEES Admin Portal'
